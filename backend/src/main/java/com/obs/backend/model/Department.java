b2890935da32ccfe5fe205b93ca57454eb81620d
Berat Zengin <devberatzengin@gmail.com>
2026-04-07 19:25:01 +0300

    feat: create Faculty and Department entities with relation

---
+++ backend/src/main/java/com/obs/backend/model/Department.java
@@ -0,0 +1,32 @@
+package com.obs.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "faculty_id", nullable = false)
+    private Faculty faculty;
+
+    @OneToMany(mappedBy = "department")
+    private List<Course> courses;
+}
---
+++ backend/src/main/java/com/obs/backend/model/Faculty.java
@@ -0,0 +1,28 @@
+package com.obs.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "faculties")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Faculty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "faculty", cascade = CascadeType.ALL)
+    private List<Department> departments;
+}
